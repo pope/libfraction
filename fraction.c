12975d2ad77698330ba80442a00c935ad67b59e1
K. Adam Christensen <pope@shifteleven.com>
2009-05-24 23:48:59 -0400

    Tons of changes.
    Changed the typedef *FUNCTION to Function.  This aligns itself more with
    everyone else's c.
    
    I also adjusted the comments in the h file.

--- fraction.c
+++ fraction.c
@@ -1,21 +1,44 @@
+// Copyright (c) 2009 K. Adam Christensen
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 #include "fraction.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-FRACTION fraction_create(int n, int d)
+Fraction* fraction_create(const int n, const int d)
 {
-    FRACTION f = (FRACTION) malloc(sizeof(FRACTION));
+    Fraction* f = (Fraction *) malloc(sizeof(Fraction));
     fraction_set(f, n, d);
     return f;
 }
 
-void fraction_set(const FRACTION f, int n, int d)
+void fraction_set(Fraction * const f, const int n, const int d)
 {
     f->numerator = n;
     f->denominator = d;
 }
 
-FRACTION fraction_add(const FRACTION a, const FRACTION b)
+Fraction * fraction_add(const Fraction * a, const Fraction * b)
 {
     int numerator = a->numerator;
     int denominator = a->denominator;
@@ -24,17 +47,17 @@ FRACTION fraction_add(const FRACTION a, const FRACTION b)
                 + denominator * b->numerator;
     denominator *= b->denominator;
 
-    FRACTION result = fraction_create(numerator, denominator);
+    Fraction * result = fraction_create(numerator, denominator);
     fraction_reduce(result);
     return result;
 }
 
-void fraction_destroy(FRACTION f)
+void fraction_destroy(Fraction * f)
 {
     free(f);
 }
 
-void fraction_reduce(const FRACTION f)
+void fraction_reduce(Fraction * const f)
 {
     int u = f->numerator;
     int v = f->denominator;
@@ -50,7 +73,7 @@ void fraction_reduce(const FRACTION f)
     f->denominator /= u;
 }
 
-void fraction_print(const FRACTION f)
+void fraction_print(const Fraction * f)
 {
     printf("%d/%d\n", f->numerator, f->denominator);
 }
--- fraction.h
+++ fraction.h
@@ -1,18 +1,85 @@
-#ifndef _FRACTION_INCLUDE_GUARD
-#define _FRACTION_INCLUDE_GUARD
+// Copyright (c) 2009 K. Adam Christensen
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
-struct _FRACTION
+//
+// A simple module for dealing with Fractions
+//
+
+#ifndef _Fraction_H
+#define _Fraction_H
+
+
+typedef struct Fraction Fraction;
+
+struct Fraction
 {
     int numerator;
     int denominator;
 };
-typedef struct _FRACTION * FRACTION;
 
-FRACTION    fraction_create(int, int);
-FRACTION    fraction_add(FRACTION, FRACTION);
-void        fraction_destroy(FRACTION);
-void        fraction_print(FRACTION);
-void        fraction_reduce(FRACTION);
-void        fraction_set(FRACTION, int, int);
+// Create a new Fraction and initialize it.
+//
+// When you no longer need the fraction, use the fraction_destroy function to
+// free the memory space.
+Fraction *
+fraction_create(
+    const int,      // numerator
+    const int       // denominator
+);
+
+// Create a new Fraction whose result is the addition of two other Fractions.
+//
+// The fraction is automatically reduced
+Fraction *
+fraction_add(
+    const Fraction *,   // Fraction a
+    const Fraction *    // Fraction b
+);
+
+// Frees up the memory used for this Fraction
+void
+fraction_destroy(
+    Fraction*
+);
+
+// Print out the Fraction
+void
+fraction_print(
+    const Fraction*
+);
+
+// Reduce the Fraction based on the GCD
+void
+fraction_reduce(
+    Fraction * const
+);
+
+// Set the numerator and denominator of the Fraction
+void
+fraction_set(
+    Fraction * const,   // the Fraction to update
+    const int,          // numerator
+    const int           // denominator
+);
 
 #endif
