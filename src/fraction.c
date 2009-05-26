371acf9f697742b6be9c193a0cca6c23dd7a7a73
K. Adam Christensen <pope@shifteleven.com>
2009-05-25 22:22:21 -0400

    Fixed the formatting some more.  Added what might be doxygen comments.

--- src/fraction.c
+++ src/fraction.c
@@ -1,45 +1,53 @@
-/* Copyright (c) 2009 K. Adam Christensen
-**
-** Permission is hereby granted, free of charge, to any person
-** obtaining a copy of this software and associated documentation
-** files (the "Software"), to deal in the Software without
-** restriction, including without limitation the rights to use,
-** copy, modify, merge, publish, distribute, sublicense, and/or sell
-** copies of the Software, and to permit persons to whom the
-** Software is furnished to do so, subject to the following
-** conditions:
-**
-** The above copyright notice and this permission notice shall be
-** included in all copies or substantial portions of the Software.
-**
-** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-** OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-** HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-** OTHER DEALINGS IN THE SOFTWARE.
-*/
+/**
+ * Copyright (c) 2009 K. Adam Christensen
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include "fraction.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-Fraction * fraction_create(const int n, const int d)
+Fraction *
+fraction_create(const int n,
+                const int d)
 {
     Fraction * f = (Fraction *) malloc(sizeof(Fraction));
     fraction_set(f, n, d);
     return f;
 }
 
-void fraction_set(Fraction * const f, const int n, const int d)
+void
+fraction_set(Fraction * const f,
+             const int n,
+             const int d)
 {
     f->numerator = n;
     f->denominator = d;
 }
 
-Fraction * fraction_add(const Fraction * a, const Fraction * b)
+Fraction *
+fraction_add(const Fraction * const a,
+             const Fraction * const b)
 {
     int numerator = a->numerator;
     int denominator = a->denominator;
@@ -53,12 +61,14 @@ Fraction * fraction_add(const Fraction * a, const Fraction * b)
     return result;
 }
 
-void fraction_destroy(Fraction * f)
+void
+fraction_destroy(Fraction * f)
 {
     free(f);
 }
 
-void fraction_reduce(Fraction * const f)
+void
+fraction_reduce(Fraction * const f)
 {
     int u = f->numerator;
     int v = f->denominator;
@@ -74,7 +84,8 @@ void fraction_reduce(Fraction * const f)
     f->denominator /= u;
 }
 
-void fraction_print(const Fraction * f)
+void
+fraction_print(const Fraction * const f)
 {
     printf("%d/%d\n", f->numerator, f->denominator);
 }
--- src/fraction.h
+++ src/fraction.h
@@ -1,88 +1,110 @@
-/* Copyright (c) 2009 K. Adam Christensen
-**
-** Permission is hereby granted, free of charge, to any person
-** obtaining a copy of this software and associated documentation
-** files (the "Software"), to deal in the Software without
-** restriction, including without limitation the rights to use,
-** copy, modify, merge, publish, distribute, sublicense, and/or sell
-** copies of the Software, and to permit persons to whom the
-** Software is furnished to do so, subject to the following
-** conditions:
-**
-** The above copyright notice and this permission notice shall be
-** included in all copies or substantial portions of the Software.
-**
-** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-** OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-** HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-** OTHER DEALINGS IN THE SOFTWARE.
-*/
+/**
+ * Copyright (c) 2009 K. Adam Christensen
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-/*
-** A simple module for dealing with Fractions
-*/
+/**
+ * A simple module for dealing with Fractions
+ */
 
-#ifndef _Fraction_H
-#define _Fraction_H
+#ifndef _FRACTION_H
+#define _FRACTION_H
 
-
+/**
+ * @brief Fraction type definition
+ */
 typedef struct Fraction Fraction;
 
+/**
+ * @brief Fraction data structure
+ */
 struct Fraction
 {
     int numerator;
     int denominator;
 };
 
-/* Create a new Fraction and initialize it.
-**
-** When you no longer need the fraction, use the fraction_destroy function to
-** free the memory space.
-*/
+/**
+ * @brief Create a new Fraction
+ *
+ * This will allocate the memory needed for a new Fraction and will initalize
+ * it.
+ *
+ * @note You must free this when you are done.
+ * @see fraction_destroy
+ *
+ * @return the pointer to the newly allocated Fraction
+ */
 Fraction *
 fraction_create(
-    const int,      /* numerator */
-    const int       /* denominator */
+    const int,      /** numerator */
+    const int       /** denominator */
 );
 
-/* Create a new Fraction whose result is the addition of two other Fractions.
-**
-** The fraction is automatically reduced
-*/
+/**
+ * @brief Add two fractions together
+ *
+ * Create a new Fraction whose result is the addition of two other Fractions.
+ * The fraction is automatically reduced
+ */
 Fraction *
 fraction_add(
-    const Fraction *,   /* Fraction a */
-    const Fraction *    /* Fraction b */
+    const Fraction * const, /** the first fraction */
+    const Fraction * const  /** the second fraction */
 );
 
-/* Frees up the memory used for this Fraction */
+/**
+ * @brief Frees up the memory used for this Fraction
+ */
 void
 fraction_destroy(
-    Fraction*
+    Fraction *
 );
 
-/* Print out the Fraction */
+/**
+ * @brief Print out the Fraction
+ */
 void
 fraction_print(
-    const Fraction*
+    const Fraction * const
 );
 
-/* Reduce the Fraction based on the GCD */
+/**
+ * @brief Reduce the Fraction
+ */
 void
 fraction_reduce(
     Fraction * const
 );
 
-/* Set the numerator and denominator of the Fraction */
+/**
+ * @brief Set the numerator and denominator of the Fraction
+ */
 void
 fraction_set(
-    Fraction * const,   /* the Fraction to update */
-    const int,          /* numerator */
-    const int           /* denominator */
+    Fraction * const,   /** the Fraction to update */
+    const int,          /** new numerator */
+    const int           /** new denominator */
 );
 
-#endif
+#endif /* _FRACTION_H */
